6310fe817cf28a01fee73e7911a578a4062eda6f
JatinKumar65042 <jatinsokhal2300@gmail.com>
2024-07-26 23:48:28 +0530

    remove one more comment

--- db.js
+++ db.js
@@ -5,9 +5,6 @@ const connectDB = async () => {
   try {
     await mongoose.connect('mongodb://127.0.0.1:27017/student_files', {
       useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    //   serverSelectionTimeoutMS: 5000, // Example: Set server selection timeout to 5 seconds
-    //   socketTimeoutMS: 45000 // Example: Set socket timeout to 45 seconds
     });
     console.log('MongoDB connected...');
   } catch (err) {
