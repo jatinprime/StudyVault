9c46541f3da4d971acb060a80c4c9e9b80237023
JatinKumar65042 <jatinsokhal2300@gmail.com>
2024-07-26 23:46:28 +0530

    undo comment remove

--- models/File.js
+++ models/File.js
@@ -1,3 +1,31 @@
+// // models/File.js
+// const mongoose = require('mongoose');
+
+// const FileSchema = new mongoose.Schema({
+//   user: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'User'
+//   },
+//   filename: {
+//     type: String,
+//     required: true
+//   },
+//   filepath: {
+//     type: String,
+//     required: true
+//   },
+//   date: {
+//     type: Date,
+//     default: Date.now
+//   }
+// });
+
+// module.exports = mongoose.model('File', FileSchema);
+
+
+
+
+
 
 
 const mongoose = require('mongoose');
